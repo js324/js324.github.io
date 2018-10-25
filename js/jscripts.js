c49ad9e5983489fc305cec4225caa4c35aebf175
js324 <jins918@gmail.com>
2018-10-24 21:51:12 -0400

    Update jscripts.js

--- js/jscripts.js
+++ js/jscripts.js
@@ -17,10 +17,12 @@ var letter=String.fromCharCode(97+slideIndex);
 
 $(".next").on("click", { d: "next" }, rotate);
 $(".prev").on("click", { d: "prev" }, rotate);
-for (var i=0;  i < dots.length; i++) 
+
+//Trying to configure dots again (potentially could be document ready at top of block)	
+/*for (var i=0;  i < dots.length; i++) 
 {
 	dots[i].addEventListener('click', currentSlide(i));
-}
+}*/
 $(".a").hover(function() {
   $(".a >"+ ".slideshowimg").css({"opacity": ".3"});
   $(".a >"+".middle").css({"opacity": ".9"});
