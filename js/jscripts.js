b2f3637ed23a3af942ae0c325092cfee4e9524a6
js324 <jins918@gmail.com>
2018-10-24 20:49:47 -0400

    Update jscripts.js

--- js/jscripts.js
+++ js/jscripts.js
@@ -17,7 +17,7 @@ var letter=String.fromCharCode(97+slideIndex);
 
 $(".next").on("click", { d: "next" }, rotate);
 $(".prev").on("click", { d: "prev" }, rotate);
-for (var i=0,  i < dots.length; i++) 
+for (var i=0;  i < dots.length; i++) 
 {
 	dots[i].onclick = currentSlides(i);
 }
